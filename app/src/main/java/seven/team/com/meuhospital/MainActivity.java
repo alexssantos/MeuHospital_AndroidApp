784bc08bd897ccc07faaa9cfba310617fc69b2e1
Alex Santos <arkanjo.alex@gmail.com>
2018-08-18 11:22:40 -0300

    Pegando Localização Atual
    pegando Localização, Marcando com botao azul, desabilitando botao padrao de Pegar Localização Atual para personalizar depois

--- app/src/main/java/seven/team/com/meuhospital/MainActivity.java
+++ app/src/main/java/seven/team/com/meuhospital/MainActivity.java
@@ -1,12 +1,9 @@
 package seven.team.com.meuhospital;
 
 import android.Manifest;
-import android.app.Activity;
 import android.app.Dialog;
-import android.app.Fragment;
-import android.content.Context;
 import android.content.pm.PackageManager;
-import android.nfc.Tag;
+import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -17,24 +14,32 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.common.server.response.FieldMappingDictionaryEntryCreator;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final  String TAG = "TAG MainActivity";
+    private static final String TAG = "TAG MainActivity";
 
-    private static final  int ERROR_DIALOG_REQUEST = 1001;
-    private static final  int LOCATION_PERMISSION_REQUEST_CODE = 1002;
+    private static final int ERROR_DIALOG_REQUEST = 1001;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1002;
     private static final String FINE_PERMISSION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COARSE_PERMISSION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final float ZOOM_PADRAO = 15f;
 
 
     //variaveis
     private boolean mPermissaoLocalPermitida = false;
     private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
 
 
     @Override
@@ -42,50 +47,87 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (servicoMapOK()){
-            pegarPermissoaLocalizacao();
+        if (servicoMapOK()) {
+            pegarPermissaoDeLocalizacao();
+            //Permissao OK -> Inicializa MAPA
+            //Permissao NAO OK -> GET Permissao + Inicializar MAPA
         }
     }
 
+    private void pegarLocalizacaoUsuario() {
+        Log.d(TAG, "pegarLocalizacaoUsuario: pegando a atual localização do usuario");
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        try {
+            if (mPermissaoLocalPermitida) {
+                //ultimalocalizacao
+                final Task localizacao = mFusedLocationProviderClient.getLastLocation();
+                localizacao.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()) {
+                            //task OK -> Pegar o resultado da localização + Posicionar Camera
+                            Log.d(TAG, "onComplete: localização encontrada");
+
+                            Location localizacaoAtual = (Location) task.getResult();
+                            moverCamera(new LatLng(localizacaoAtual.getLatitude(), localizacaoAtual.getLongitude()), ZOOM_PADRAO);
+
+                        } else {
+                            Log.d(TAG, "onComplete: localização atual está Null");
+                            Toast.makeText(getApplicationContext(), "Localização atual não acessivel", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+            }
+
+        } catch (SecurityException e) {
+            Log.d(TAG, "pegarLocalizacaoUsuario: SecurityException: " + e.getMessage());
+        }
+    }
+
+    private void moverCamera(LatLng latLng, float zoom) {
+        Log.d(TAG, "moverCamera: movendo a camera para: Lat: " + latLng.latitude + ", Long: " + latLng.longitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+    }
+
     //Verficação da conexaxao do Google Services
-    public boolean servicoMapOK(){
+    public boolean servicoMapOK() {
         Log.d(TAG, "requestServiceMapOK: checking google services version");
         int disponivel = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
 
-        if (disponivel == ConnectionResult.SUCCESS){
+        if (disponivel == ConnectionResult.SUCCESS) {
             //TUDO OK
             Log.d(TAG, "requestServiceMapOK: Google Play Services está funcionando");
             return true;
-        }
-        else if (GoogleApiAvailability.getInstance().isUserResolvableError(disponivel)){
+        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(disponivel)) {
             //Tem um erro porem é consertavel.
-            Log.d(TAG,"requestServiceMapOK: Erro na requisição de Mapa ao Google Services (pode ser consertado)");
+            Log.d(TAG, "requestServiceMapOK: Erro na requisição de Mapa ao Google Services (pode ser consertado)");
             Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, disponivel, ERROR_DIALOG_REQUEST);
             dialog.show();
-        }
-        else {
-            Toast.makeText(this,"Mapa não Autorizado. \n Você não pode fazer uma requisição de Mapa", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Mapa não Autorizado. \n Você não pode fazer uma requisição de Mapa", Toast.LENGTH_SHORT).show();
         }
         return false;
     }
 
-    private void pegarPermissoaLocalizacao(){
-        Log.d(TAG, "pegarPermissoaLocalizacao: verificar permissoes de localização");
-        String [] permissoes = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+    private void pegarPermissaoDeLocalizacao() {
+        Log.d(TAG, "pegarPermissaoDeLocalizacao: verificar permissoes de localização");
+        String[] permissoes = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
 
         //Verificar Permissoes de Localização
-        if (ContextCompat.checkSelfPermission(getApplicationContext(), FINE_PERMISSION) == PackageManager.PERMISSION_GRANTED){
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), FINE_PERMISSION) == PackageManager.PERMISSION_GRANTED) {
 
-            if (ContextCompat.checkSelfPermission(getApplicationContext(), COARSE_PERMISSION) == PackageManager.PERMISSION_GRANTED){
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), COARSE_PERMISSION) == PackageManager.PERMISSION_GRANTED) {
                 Log.d(TAG, "pegarPermissaoLocalizacao: Permissoes Concedidas.");
                 mPermissaoLocalPermitida = true;
-            }
-            else{
+                inicializarMapa();
+            } else {
                 //Pedir Permissoes
                 ActivityCompat.requestPermissions(this, permissoes, LOCATION_PERMISSION_REQUEST_CODE);
             }
-        }
-        else{
+        } else {
             //Pedir Permissoes
             ActivityCompat.requestPermissions(this, permissoes, LOCATION_PERMISSION_REQUEST_CODE);
         }
@@ -95,12 +137,12 @@ public class MainActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         mPermissaoLocalPermitida = false;
 
-        switch(requestCode){
+        switch (requestCode) {
             case LOCATION_PERMISSION_REQUEST_CODE:
                 //se alguma permissao foi dada
-                if (grantResults.length > 0){
-                    for (int i = 0; i < grantResults.length; i++ ){
-                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
                             mPermissaoLocalPermitida = false;
                             Log.d(TAG, "onRequestPermissionResult: permissao negada.");
                             return;
@@ -114,7 +156,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void inicializarMapa(){
+    private void inicializarMapa() {
         Log.d(TAG, "inicializarMap: inicializando mapa");
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
 
@@ -122,9 +164,23 @@ public class MainActivity extends AppCompatActivity {
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
-                Toast.makeText(getApplicationContext(), "Mapa Pronto!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Permissao Garantida. \n   Mapa Pronto!", Toast.LENGTH_LONG).show();
                 Log.d(TAG, "onMapReady: mapa está pronto");
                 mMap = googleMap;
+
+                //Pegar Localização Atual do Usuario
+                pegarLocalizacaoUsuario();
+
+                //Marcar localização Atual Usuario
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(),
+                            Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                        && ActivityCompat.checkSelfPermission(getApplicationContext(),
+                            Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                
             }
         });
     }
