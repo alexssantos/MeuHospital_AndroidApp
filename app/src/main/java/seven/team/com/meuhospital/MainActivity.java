207d8e99468a4e47db55cdec063ea23a9b7c5ed1
Alex Santos <arkanjo.alex@gmail.com>
2018-08-18 10:29:15 -0300

    Implementado Permissoes de Localização

--- app/src/main/java/seven/team/com/meuhospital/MainActivity.java
+++ app/src/main/java/seven/team/com/meuhospital/MainActivity.java
@@ -1,13 +1,133 @@
 package seven.team.com.meuhospital;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.nfc.Tag;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.server.response.FieldMappingDictionaryEntryCreator;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final  String TAG = "TAG MainActivity";
+
+    private static final  int ERROR_DIALOG_REQUEST = 1001;
+    private static final  int LOCATION_PERMISSION_REQUEST_CODE = 1002;
+    private static final String FINE_PERMISSION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_PERMISSION = Manifest.permission.ACCESS_COARSE_LOCATION;
+
+
+    //variaveis
+    private boolean mPermissaoLocalPermitida = false;
+    private GoogleMap mMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (servicoMapOK()){
+            pegarPermissoaLocalizacao();
+        }
     }
+
+    //Verficação da conexaxao do Google Services
+    public boolean servicoMapOK(){
+        Log.d(TAG, "requestServiceMapOK: checking google services version");
+        int disponivel = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if (disponivel == ConnectionResult.SUCCESS){
+            //TUDO OK
+            Log.d(TAG, "requestServiceMapOK: Google Play Services está funcionando");
+            return true;
+        }
+        else if (GoogleApiAvailability.getInstance().isUserResolvableError(disponivel)){
+            //Tem um erro porem é consertavel.
+            Log.d(TAG,"requestServiceMapOK: Erro na requisição de Mapa ao Google Services (pode ser consertado)");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, disponivel, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }
+        else {
+            Toast.makeText(this,"Mapa não Autorizado. \n Você não pode fazer uma requisição de Mapa", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    private void pegarPermissoaLocalizacao(){
+        Log.d(TAG, "pegarPermissoaLocalizacao: verificar permissoes de localização");
+        String [] permissoes = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        //Verificar Permissoes de Localização
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), FINE_PERMISSION) == PackageManager.PERMISSION_GRANTED){
+
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), COARSE_PERMISSION) == PackageManager.PERMISSION_GRANTED){
+                Log.d(TAG, "pegarPermissaoLocalizacao: Permissoes Concedidas.");
+                mPermissaoLocalPermitida = true;
+            }
+            else{
+                //Pedir Permissoes
+                ActivityCompat.requestPermissions(this, permissoes, LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        }
+        else{
+            //Pedir Permissoes
+            ActivityCompat.requestPermissions(this, permissoes, LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        mPermissaoLocalPermitida = false;
+
+        switch(requestCode){
+            case LOCATION_PERMISSION_REQUEST_CODE:
+                //se alguma permissao foi dada
+                if (grantResults.length > 0){
+                    for (int i = 0; i < grantResults.length; i++ ){
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                            mPermissaoLocalPermitida = false;
+                            Log.d(TAG, "onRequestPermissionResult: permissao negada.");
+                            return;
+                        }
+                    }
+                }
+                Log.d(TAG, "onRequestPermissionResult: permissao consedida.");
+                mPermissaoLocalPermitida = true;
+                //Permissoes OK - inicializar o mapa
+                inicializarMapa();
+        }
+    }
+
+    private void inicializarMapa(){
+        Log.d(TAG, "inicializarMap: inicializando mapa");
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        //Configurar e inicializar o mapa
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                Toast.makeText(getApplicationContext(), "Mapa Pronto!", Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "onMapReady: mapa está pronto");
+                mMap = googleMap;
+            }
+        });
+    }
+
+
 }
